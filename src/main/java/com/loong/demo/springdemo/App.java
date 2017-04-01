e5251b2a9172f39964dfd6985a6eb488de2785e5
huangjunlong <huangjunlong@CShell>
2017-04-01 15:51:50 +0800

    add

---
+++ src/main/java/com/loong/demo/springdemo/App.java
@@ -0,0 +1,13 @@
+package com.loong.demo.springdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
